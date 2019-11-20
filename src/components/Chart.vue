64c91b19b701a6d1966fdd19bced49ee9afa1d72
mark-drastrup <mark.drastrup@gmail.com>
2019-11-20 23:48:04 +0900

    Adding arrows and edge labels

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -19,24 +19,40 @@ export default {
             .attr("transform",
               `translate(${40}, ${10})`)
 
-        const edges = svg
+        this.edges = svg
           .selectAll("line")
-          .data(this.links)
+          .data(this.linksArr)
           .enter()
           .append("line")
             .attr("stroke", "black")
             .attr("stroke-width", 2)
 
-        const nodes = svg
+        this.nodes = svg
           .selectAll("circle")
-          .data(this.nodes)
+          .data(this.nodesArr)
           .enter()
           .append("circle")
             .attr("r", 20)
             .attr("fill", "red")
+            .style("cursor", "pointer")
+            .on("mouseover", function() {
+              d3.select(this).transition()
+                .duration(250)
+                .attr("r", 25)
+            })
+            .on("mouseout", function() {
+              d3.select(this).transition()
+                .duration(250)
+                .attr("r", 20)
+            })
+            .call(d3.drag()
+              .on("start", this.dragStart)
+              .on("drag", this.drag)
+              .on("end", this.dragEnd)
+            ) 
             
-        const edgepaths = svg.selectAll(".edgepath")
-          .data(this.links)
+        this.edgepaths = svg.selectAll(".edgepath")
+          .data(this.linksArr)
           .enter()
           .append('path')
           .attr("class","edgepath")
@@ -45,29 +61,95 @@ export default {
           .attr("fill", "black")
           .attr("stroke", "black")
           .attr("id", (d, i) => `edgepath${i}`)
+          .attr('marker-end','url(#arrowhead)')
+            .style("stroke","#ccc")
+            .style("pointer-events", "none");
 
-        const simulation = d3.forceSimulation(this.nodes)
+        this.edgelabels = svg.selectAll(".edgelabel")
+          .data(this.linksArr)
+          .enter()
+          .append('text')
+          .style("pointer-events", "none")
+          .attr("class", "edgelabel")
+          .attr("id", (d, i) => `edgelabel${i}`)
+          .attr("dy", -2)
+          .attr("font-size", 12)
+          .attr("fill", "black")
+
+        this.edgelabels.append('textPath')
+          .attr('xlink:href',function(d,i) {return '#edgepath'+i})
+          .attr("startOffset", "50%")
+          .style("text-anchor", "middle")
+          .style("pointer-events", "none")
+          .text(function(d,i){return 'label '+i});
+
+        svg.append('defs').append('marker')
+          .attr("id", "arrowhead")
+          .attr("viewBox", "-0 -5 10 10")
+          .attr("refX", 30)
+          .attr("refY", 0)
+          .attr("orient", "auto")
+          .attr("markerWidth", 10)
+          .attr("markerHeight", 10)
+          .attr("xoverflow", "visible")
+          .append('svg:path')
+              .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+              .attr('fill', "black")
+              .attr('stroke',"black");
+
+        this.simulation = d3.forceSimulation(this.nodesArr)
           .force("link", d3.forceLink()
             .id(d => d.id)
-            .links(this.links).distance(80).strength(1)
+            .links(this.linksArr).distance(80).strength(1)
           )
           .force("charge", d3.forceManyBody().strength(-2000))
           .force("center", d3.forceCenter(this.width / 2, this.height / 2))
-          .on("tick", () => this.ticked(edges, nodes))
+          .on("tick", this.ticked)
 
     },
     methods: {  
-      ticked(edges, nodes) {
-        /* console.log('called') */
-        edges
+      ticked() {
+        this.edges
           .attr("x1", d => d.source.x)
           .attr("y1", d => d.source.y)
           .attr("x2", d => d.target.x)
           .attr("y2", d => d.target.y)
 
-         nodes
-        .attr("cx", d => d.x )
-        .attr("cy", d => d.y )
+         this.nodes
+          .attr("cx", d => d.x )
+          .attr("cy", d => d.y )
+
+        this.edgepaths.attr("d", d => {
+          const path = 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
+          return path
+        })
+
+        
+        /* this.edgelabels
+          .attr('transform',function(d,i) {
+            if (d.target.x<d.source.x) {
+              bbox = this.getBBox();
+              rx = bbox.x+bbox.width/2;
+              ry = bbox.y+bbox.height/2;
+              return 'rotate(180 '+rx+' '+ry+')';
+            } else {
+              return 'rotate(0)';
+            }
+          }) */
+      },
+      dragStart(d) {
+        this.simulation.alphaTarget(0.5).restart()
+        d.fx = d.x;
+        d.fy = d.y;
+      },
+      drag(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+      },
+      dragEnd(d) {
+        this.simulation.alphaTarget(0)
+        d.fx = null;
+        d.fy = null
       }
     },
     data: () => ({
@@ -79,7 +161,7 @@ export default {
             left: 40,
             right: 40
         },
-        nodes: [
+        nodesArr: [
             { id: 1, name: "A", type: "primary" },
             { id: 2, name: "B", type: "primary" },
             { id: 3, name: "C", type: "danger" },
@@ -91,7 +173,7 @@ export default {
             { id: 9, name: "I", type: "secondary" },
             { id: 10, name: "J", type: "primary" }
         ],
-        links: [
+        linksArr: [
             { source: 1, target: 2, value: 12 },
             { source: 1, target: 5, value: 5 },
             { source: 1, target: 6, value: 13 },
@@ -102,7 +184,13 @@ export default {
             { source: 4, target: 5, value: 3 },
             { source: 4, target: 9, value: 3 },
             { source: 5, target: 10, value: 7 }
-        ]
+        ],
+        nodes:null,
+        edges: null,
+        labels: null,
+        simulation: null,
+        edgepaths: null,
+        edgelabels: null
     })
 }
 </script>
